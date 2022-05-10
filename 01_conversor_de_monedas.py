04cdb66e8831b581614b372102e6e792af8437bf
Santiago Arias <sanargo97@gmail.com>
2022-05-10 16:35:32 -0500

    se añaden algunos proyectos del curso python Intermedio, y se modifican las identaciones

--- 01_conversor_de_monedas.py
+++ 01_conversor_de_monedas.py
@@ -1,12 +1,9 @@
-# En este proyecto obtendremos 
-
-
 def conversor(tipo_pesos, valor_dolar):
-  pesos = float(input('¿Cuantos pesos ' + tipo_pesos + ' tienes?: '))
-  dolares = pesos / valor_dolar
-  dolares = round(dolares, 2)
-  dolares = str(dolares)
-  print('Tienes $' + dolares + ' dólares')
+    pesos = float(input('¿Cuantos pesos ' + tipo_pesos + ' tienes?: '))
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print('Tienes $' + dolares + ' dólares')
 
 menu = """
 Bienvenido al conversor de monedas 
@@ -20,8 +17,8 @@ Elige una opción: """
 opcion = int(input(menu))
 
 if opcion == 1:
-  conversor('colombianos', 3875)
+    conversor('colombianos', 3875)
 elif opcion == 2:
-  conversor('argentinos', 65)
+    conversor('argentinos', 65)
 elif opcion == 3:
-  conversor('mexicanos', 24)
+    conversor('mexicanos', 24)
