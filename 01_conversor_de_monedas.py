ed32d65d0af47f4119a9a8b945a76df345dff909
Santiago Arias <sanargo97@gmail.com>
2022-05-10 16:04:25 -0500

    Se agregan todos los proyectos realizados en el curso de Python básico

---
+++ 01_conversor_de_monedas.py
@@ -0,0 +1,27 @@
+# En este proyecto obtendremos 
+
+
+def conversor(tipo_pesos, valor_dolar):
+  pesos = float(input('¿Cuantos pesos ' + tipo_pesos + ' tienes?: '))
+  dolares = pesos / valor_dolar
+  dolares = round(dolares, 2)
+  dolares = str(dolares)
+  print('Tienes $' + dolares + ' dólares')
+
+menu = """
+Bienvenido al conversor de monedas 
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+  conversor('colombianos', 3875)
+elif opcion == 2:
+  conversor('argentinos', 65)
+elif opcion == 3:
+  conversor('mexicanos', 24)
